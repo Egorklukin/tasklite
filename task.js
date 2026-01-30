5c8eabdc31108032beb7fb08bf684d3a1032fc9d
Egor <klukinegor2@gmail.com>
2026-01-30 17:55:29 +0300

    styles

--- task.js
+++ task.js
@@ -5,3 +5,20 @@ let difference = a - b;
 
 console.log("сумма " + sum);
 console.log("разность " + difference);
+
+let title = "";
+if (title === "") {
+    console.log("Заголовок пустой")
+} else {
+    console.log("title: " + title)
+}
+
+let tasks = 7;
+
+if (tasks === 0) {
+    console.log("Список пуст, можно отдыхать");
+} else if (tasks > 0 && tasks <= 3) {
+    console.log("Есть задачи для выполнения");
+} else if (tasks > 3) {
+    console.log("Список задач переполнен");
+}
