5c87ee1adc060ff5e806331b82484cb860a8459c
Egor <klukinegor2@gmail.com>
2026-03-10 16:54:05 +0300

    HW #14

--- main.js
+++ main.js
@@ -1,153 +1,159 @@
-const input = document.querySelector(".form-add__input")
-const addButton = document.querySelector(".form-add__btn")
-const container = document.querySelector(".tasks")
-const searchInput = document.querySelector(".toolbar__input")
-const footer = document.querySelector(".footer-controls")
-const sortSelect = document.querySelector(".toolbar__sort")
-const tabButtons = document.querySelectorAll(".tabs__item")
-const clearButton = document.querySelector(".footer-controls__clear")
-const form = document.querySelector(".form-add")
+const input = document.querySelector(".form-add__input");
+const addButton = document.querySelector(".form-add__btn");
+const container = document.querySelector(".tasks");
+const searchInput = document.querySelector(".toolbar__input");
+const footer = document.querySelector(".footer-controls");
+const sortSelect = document.querySelector(".toolbar__sort");
+const tabButtons = document.querySelectorAll(".tabs__item");
+const clearButton = document.querySelector(".footer-controls__clear");
+const form = document.querySelector(".form-add");
 const tasks = [
-    {
-        date: "Сегодня, 10:00",
-        text: "Проверить и ответить на важные электронные письма",
-        done: true
-    },
-    {
-        date: "Сегодня, 14:30",
-        text: "Провести встречу с командой разработки по планированию спринта",
-        done: false
-    },
-    {
-        date: "Завтра, 09:15",
-        text: "Подготовить презентацию для клиентского совещания",
-        done: false
-    },
-    {
-        date: "Завтра, 12:00",
-        text: "Отправить отчёт по продажам за прошлую неделю",
-        done: true
-    },
-    {
-        date: "12 мая, 16:00",
-        text: "Обновить документацию по API",
-        done: false
-    },
-    {
-        date: "11 мая, 13:45",
-        text: "Протестировать новую версию мобильного приложения",
-        done: true
-    },
-    {
-        date: "Сегодня, 18:00",
-        text: "Запланировать отпуск на следующий квартал",
-        done: false
-    }
+  {
+    date: "Сегодня, 10:00",
+    text: "Проверить и ответить на важные электронные письма",
+    done: true,
+  },
+  {
+    date: "Сегодня, 14:30",
+    text: "Провести встречу с командой разработки по планированию спринта",
+    done: false,
+  },
+  {
+    date: "Завтра, 09:15",
+    text: "Подготовить презентацию для клиентского совещания",
+    done: false,
+  },
+  {
+    date: "Завтра, 12:00",
+    text: "Отправить отчёт по продажам за прошлую неделю",
+    done: true,
+  },
+  {
+    date: "12 мая, 16:00",
+    text: "Обновить документацию по API",
+    done: false,
+  },
+  {
+    date: "11 мая, 13:45",
+    text: "Протестировать новую версию мобильного приложения",
+    done: true,
+  },
+  {
+    date: "Сегодня, 18:00",
+    text: "Запланировать отпуск на следующий квартал",
+    done: false,
+  },
 ];
 
-
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    addTask()
-})
+  event.preventDefault();
+  addTask();
+});
 
 function addTask() {
-    const text = input.value.trim()
-    if (text === "" || text.length < 3) {
-        input.classList.add("input--error")
-        return
-    }
+  const text = input.value.trim();
+  if (text === "" || text.length < 3) {
+    input.classList.add("input--error");
+    return;
+  }
 
-    input.classList.remove("input--error")
+  input.classList.remove("input--error");
 
-    const newTask = {
-        id: tasks.length + 1,
-        text: text,
-        done: false,
-        date: "Завтра 11.04.26"
-    }
-    tasks.push(newTask)
+  const newTask = {
+    id: Date.now(),
+    text: text,
+    done: false,
+    date: formatterDate(new Date()),
+  };
+  tasks.push(newTask);
+  input.value = "";
 
-    input.value = ""
-
-    renderAll()
+  renderAll();
 }
 function renderTask(task) {
-    const item = document.createElement("div")
-    item.classList.add("task")
+  const item = document.createElement("div");
+  item.classList.add("task");
 
-    const content = document.createElement("div")
-    content.classList.add("task__content")
+  const content = document.createElement("div");
+  content.classList.add("task__content");
 
+  const title = document.createElement("div");
+  title.classList.add("task__title");
+  title.textContent = task.text;
 
-    const title = document.createElement("div")
-    title.classList.add("task__title")
-    title.textContent = task.text
+  const meta = document.createElement("div");
+  meta.classList.add("task__meta");
+  meta.textContent = task.date;
 
-    const meta = document.createElement("div")
-    meta.classList.add("task__meta")
-    meta.textContent = task.date
+  content.append(title, meta);
 
-    content.append(title, meta)
-
-    const actions = document.createElement("div")
-    actions.classList.add("task__actions")
-
-    const editBtn = document.createElement("button")
-    editBtn.classList.add("task__action", "task__action--edit")
-    editBtn.innerHTML = `<svg class="task__icon" viewBox="0 0 24 24" width="14" height="14" fill="none" stroke="#6f64a3"
+  const actions = document.createElement("div");
+  actions.classList.add("task__actions");
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("task__action", "task__action--edit");
+  editBtn.innerHTML = `<svg class="task__icon" viewBox="0 0 24 24" width="14" height="14" fill="none" stroke="#6f64a3"
                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M12 20h9" />
                         <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" />
-                    </svg>`
+                    </svg>`;
 
-    editBtn.addEventListener("click", () => {
-        const newText = prompt("Изменить задачу: ", task.text)
-        if (newText && newText.trim() !== '') {
-            task.text = newText.trim()
-            renderAll()
-        }
-        console.log(newText, task.text)
-    })
+  editBtn.addEventListener("click", () => {
+    const newText = prompt("Изменить задачу: ", task.text);
+    if (newText && newText.trim() !== "") {
+      task.text = newText.trim();
+      renderAll();
+    }
+    console.log(newText, task.text);
+  });
 
-    const deleteBtn = document.createElement("button")
-    deleteBtn.classList.add("task__action", "task__action--delete")
-    deleteBtn.innerHTML = `<svg class="task__icon" viewBox="0 0 24 24" width="14 " height="14" fill="none" stroke="#cb6e6e"
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("task__action", "task__action--delete");
+  deleteBtn.innerHTML = `<svg class="task__icon" viewBox="0 0 24 24" width="14 " height="14" fill="none" stroke="#cb6e6e"
                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <polyline points="3 6 5 6 21 6" />
                         <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
                         <path d="M10 11v6" />
                         <path d="M14 11v6" />
                         <path d="M9 6V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2" />
-                    </svg>`
+                    </svg>`;
 
-    deleteBtn.addEventListener("click", () => {
-        const index = tasks.indexOf(task)
-        tasks.splice(index, 1)
-        renderAll()
-    })
+  deleteBtn.addEventListener("click", () => {
+    const index = tasks.indexOf(task);
+    tasks.splice(index, 1);
+    renderAll();
+  });
 
-    actions.append(editBtn, deleteBtn)
-    item.append(content, actions)
+  actions.append(editBtn, deleteBtn);
+  item.append(content, actions);
 
-    if (task.done) {
-        item.classList.add("task--done")
-    }
-    item.addEventListener("click", (e) => {
-        if (e.target.closest(".task__action")) return;
-        task.done = !task.done
-        console.log(task.done)
-        renderAll()
-    })
-    return item;
+  if (task.done) {
+    item.classList.add("task--done");
+  }
+  item.addEventListener("click", (e) => {
+    if (e.target.closest(".task__action")) return;
+    task.done = !task.done;
+    console.log(task.done);
+    renderAll();
+  });
+  return item;
 }
 
 function renderAll() {
-    document.querySelectorAll(".task").forEach(t => t.remove())
-    tasks.forEach((task) => {
-        const card = renderTask(task)
-        footer.before(card)
-    })
+  document.querySelectorAll(".task").forEach((t) => t.remove());
+  tasks.forEach((task) => {
+    const card = renderTask(task);
+    footer.before(card);
+  });
 }
 
-renderAll()
+function formatterDate(date) {
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+  const h = date.getHours().toString().padStart(2, "0");
+  const m = date.getMinutes().toString().padStart(2, "0");
+
+  return `${day}.${month}.${year}, ${h}:${m}`;
+}
+
+renderAll();
--- task.js
+++ task.js
@@ -1,244 +1,36 @@
-let a = 1;
-let b = 0;
-let sum = a + b;
-let difference = a - b;
+const now = new Date();
+console.log(now);
 
-console.log("сумма " + sum);
-console.log("разность " + difference);
-/* lesson 2*/
-let title = "";
-if (title === "") {
-    console.log("Заголовок пустой")
+const day = now.getDate();
+const month = now.getMonth() + 1;
+const year = now.getFullYear();
+
+console.log(`${day}.${month}.${year}`);
+
+const h = now.getHours();
+const m = now.getMinutes();
+const s = now.getSeconds();
+
+console.log(`${h}:${m}:${s}`);
+
+console.log(now.toLocaleString());
+
+const days = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
+
+const dayName = days[now.getDay()];
+
+console.log(dayName);
+
+let timeOfDay;
+
+if (h > 5 && h < 12) {
+  timeOfDay = "Утро";
+} else if (h >= 12 && h < 17) {
+  timeOfDay = "День";
+} else if (h >= 17 && h < 21) {
+  timeOfDay = "Вечер";
 } else {
-    console.log("title: " + title)
+  timeOfDay = "Ночь";
 }
 
-let tasks_count = 7;
-
-if (tasks_count === 0) {
-    console.log("Список пуст, можно отдыхать");
-} else if (tasks_count > 0 && tasks_count <= 3) {
-    console.log("Есть задачи для выполнения");
-} else if (tasks_count > 3) {
-    console.log("Список задач переполнен");
-}
-
-let isCompleted;
-
-if (isCompleted === true) {
-    console.log("Задача выполнена")
-} else {
-    console.log("Задача ещё в работе")
-}
-
-let urgent;
-
-if (tasks_count > 0 && urgent > 0) {
-    console.log("Есть срочные задачи")
-} else if (tasks_count > 0 && urgent === 0) {
-    console.log("Задачи есть, но они не срочные")
-} else if (tasks_count === 0) {
-    console.log("Все задачи завершены")
-}
-let isAdmin;
-let isModerator;
-
-if (isAdmin === true || isModerator === true) {
-    console.log("Доступ разрешён")
-} else {
-    console.log("Доступ запрещён")
-}
-
-let amount = 0;
-
-if (amount === 0) {
-    console.log("Корзина пуста");
-} else if (amount < 1000) {
-    console.log("Скидка не применяется");
-} else if (amount >= 1000 && amount < 5000) {
-    console.log("Скидка 5%");
-} else if (amount >= 5000) {
-    console.log("Скидка 10%");
-}
-/* lesson 3*/
-function summa(a, b) {
-    return a + b;
-}
-console.log("суммы: " + summa(2, 3), summa(4, 1))
-
-function isTaskDone(status) {
-    return status === "выполнена"
-}
-
-let result = isTaskDone("выполнена")
-
-console.log(result)
-
-function taskSummary(total, done) {
-    let active = total - done
-    return "Всего: " + total + "| Активных: " + active + "| Выполненых: " + done
-}
-
-console.log(taskSummary(6, 3))
-
-/* lesson 4*/
-
-let cities = ["Нарьян-Мар", "Уфа", "Новороссийск"]
-
-cities[1] = "Киров"
-console.log(cities)
-
-let task = {
-    id: 1,
-    title: "накормить собаку",
-    status: "активна"
-}
-console.log(task.title)
-task.status = "выполнена"
-console.log(task.status)
-
-let tasks1 = [
-    {
-        id: 1,
-        title: "накормить собаку",
-        status: "активна"
-    },
-    {
-        id: 2,
-        title: "купить лопату",
-        status: "выполнена"
-    },
-    {
-        id: 3,
-        title: "сходить в стр. магазин",
-        status: "не активна"
-    },
-    {
-        id: 4,
-        title: "накормить кота",
-        status: "активна"
-    },
-    {
-        id: 5,
-        title: "купить топор",
-        status: "выполнена"
-    },
-    {
-        id: 6,
-        title: "сходить в хоз. магазин",
-        status: "не активна"
-    },
-]
-console.log(tasks1[0].title)
-console.log(tasks1[1].status)
-
-let user = {
-    name: "Иван",
-    tasks: tasks1
-}
-console.log(user)
-
-function findTaskByTitle(tasks, title) {
-    for (let i = 0; i < tasks.lenght; i++) {
-        if (tasks[i].title === title) {
-            return tasks[i]
-        }
-    }
-    return "Задача не найдена"
-}
-
-console.log(findTaskByTitle(tasks1, "купить лопату"))
-
-
-function filterByStatus(tasks, status) {
-    return tasks.filter(function (task) {
-        return task.status === status;
-    })
-}
-console.log(filterByStatus(tasks1, "активна"))
-
-function sortByTitle(tasks) {
-    tasks.sort(function(a, b) {
-        if (a.title > b.title) {
-            return 1
-        }
-        if (a.title < b.title) {
-            return -1
-        }
-        return 0
-    })
-    return tasks
-}
-
-console.log(sortByTitle(tasks1))
-
-function searchByTitle(tasks, query) {
-    let q = query.toLowerCase();
-    return tasks.filter(task =>
-        task.title.toLowerCase().indexOf(q) !== -1
-    )
-}
-
-console.log(searchByTitle(tasks1, "ку"))
-
-for (let i = 0; i < tasks1.length; i++) {
-    console.log(tasks1[i].id + ":" + tasks1[i].title)
-}
-
-for(let task of tasks1) {
-    console.log(task.id + ":" + task.title)
-}
-
-let i = 0
-let total = 0
-let active = 0
-let done = 0
-
-while (i < tasks1.length) {
-    total++
-    if (tasks1[i].status === "выполнена") {
-        done++
-    } else {
-        active++
-    }
-    i++
-}
-
-console.log("Всего: " + total + " | Активных: " + active + " | Выполненых: " + done)
-
-for (let task of tasks1) {
-    if (task.status === "активна") {
-        console.log("Активная задача: " + task.title)
-    }
-}
-
-tasks1.forEach(task => {
-    console.log("#" + task.id + " " + task.title + " (" + task.status + ")")
-})
-
-let searchTitle = "купить лопату"
-let found = null
-for (let task of tasks1) {
-    if (task.title === searchTitle) {
-        found = task
-        break
-    }
-}
-
-if (found) {
-    console.log("Найденая задача: ", found)
-} else {
-    console.log("Задача не найдена")
-}
-
-function findTaskByKeyword(tasks, keyword) {
-  const lowerKeyword = keyword.toLowerCase()
-  for (const task of tasks) {
-    if (task.title.toLowerCase().includes(lowerKeyword)) {
-      return task;
-    }
-  }
-  return "Задача не найдена"
-}
-
-console.log(findTaskByKeyword(tasks1, "соба"))
+console.log(timeOfDay);
